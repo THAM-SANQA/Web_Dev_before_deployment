3ecf557ce0c032ffb5697b0201889af84c52d450
THAM-SANQA <ndabat020@gmail.com>
2024-02-24 12:58:27 +0200

    movies

--- Mern_Stack/movie-reviews/backend/api/movies.route.js
+++ Mern_Stack/movie-reviews/backend/api/movies.route.js
@@ -1,24 +1,23 @@
-import express from 'express'
-//import MoviesController from './movies.controller.js'
-//import ReviewsController from './reviews.controller.js' 
+import express from "express";
+import MoviesController from "./movies.controller.js";
+import ReviewsController from "./reviews.controller.js";
 
 // get access to express router
-const router = express.Router() 
+const router = express.Router();
 
-//router.route('/').get((req,res) => res.send('hello world'))
-//gets full movie object
-//router.route('/').get(MoviesController.apiGetMovies)
-router.route('/').get((req,res) => res.send('hello world'))
+//router.route("/").get((req, res) => res.send("hello world"));
+////gets full movie object
+router.route("/").get(MoviesController.apiGetMovies);
 
 // //route gets specific movies and their ratings
-// router.route("/id/:id").get(MoviesController.apiGetMovieById)
-// router.route("/ratings").get(MoviesController.apiGetRatings)
+router.route("/id/:id").get(MoviesController.apiGetMovieById);
+router.route("/ratings").get(MoviesController.apiGetRatings);
 
 // //CRUD functioning for reviews
-// router
-//     .route("/review")
-//     .post(ReviewsController.apiPostReview)
-//     .put(ReviewsController.apiUpdateReview)
-//     .delete(ReviewsController.apiDeleteReview)
+router
+  .route("/review")
+  .post(ReviewsController.apiPostReview)
+  .put(ReviewsController.apiUpdateReview)
+  .delete(ReviewsController.apiDeleteReview);
 
-export default router
+export default router;
